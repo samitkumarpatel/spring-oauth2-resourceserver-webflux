d5872e0a3275467bfc8a1ca9b84c002ac832ef48
samitkumarpatel <samitkumarpatel@gmail.com>
2025-03-21 15:09:13 +0000

    first commit

---
+++ src/main/java/net/samitkumar/spring_oauth2_resource_api/SpringOauth2ResourceApiApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.spring_oauth2_resource_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ResourceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ResourceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/spring_oauth2_resource_api/SpringOauth2ResourceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.spring_oauth2_resource_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
